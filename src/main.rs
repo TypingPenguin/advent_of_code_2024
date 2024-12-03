99bf1c4e6a9dc0895d696f6b3bff9ceabe2059ac
TypingPenguin <floris.kromwijk@gmail.com>
2024-12-03 12:43:40 +0100

    This took way too long to do in rust... That was just completely unnecessary parsing of text...

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ use log::debug;
 mod helper_functions;
 mod template;
 mod day_1;
+mod day_3;
 
 fn main() {
     // Initialize the logger
     env_logger::builder().filter_level(log::LevelFilter::Debug).init();
 
-    day_1::day::run();
+    // day_1::day::run();
+
+    day_3::day::run();
 
 }
